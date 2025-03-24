5bc43013235a1d75fa4dd65aed9dd8b4cc9b4ee7
Shawn Matthiessen <shawnbon206@gmail.com>
2025-03-23 19:41:20 -0700

    main/crispy-doom: get rid of most .desktop files, keep the Doom one

--- main/crispy-doom/template.py
+++ main/crispy-doom/template.py
@@ -16,3 +16,14 @@ url = "https://github.com/fabiangreffrath/crispy-doom"
 source = f"{url}/archive/crispy-doom-{pkgver}.tar.gz"
 sha256 = "25eea88fdbe1320ad0d1a3e0ed66ae8d985c39b79e442beab5fc36d9d5ddfc42"
 hardening = ["vis", "!cfi", "!int"]
+
+def post_install(self):
+    # Remove all .desktop except the Doom one
+    for f in [
+        "usr/share/applications/*Heretic.desktop",
+        "usr/share/applications/*Hexen.desktop",
+        "usr/share/applications/*Strife.desktop",
+    ]:
+        self.uninstall(f, glob=True)
+
+            
